fa396c01a9fd49ce7a220f125303dc799f529d00
Mahmoud Hamza <mahmoudmhamza96@gmail.com>
2021-01-13 01:46:14 +0200

    Update elevator task

--- Multitasking elevator simulation application/Windows Form/Form1.cs
+++ Multitasking elevator simulation application/Windows Form/Form1.cs
@@ -10,42 +10,64 @@ using System.Windows.Forms;
 using System.IO.Ports;
 using System.Threading;
 
-
 namespace Task2_Elevator
 {
     public partial class Form1 : Form
     {
+        private const int FirstFloorIndex = 1;
+        private const int SecondFloorIndex = 2;
+        private const int ThirdFloorIndex = 3;
+        private const int ForthFloorIndex = 4;
+        private const int FifthFloorIndex = 5;
+
+        /// <summary>
+        /// Determines elevator's different states
+        /// </summary>
+        private enum ElevatorState
+        {
+            Idle,
+            Up,
+            Down
+        }
+
+        /// <summary>
+        /// Port that will be used in arduino connection
+        /// </summary>
         private SerialPort myPort;
 
-        //Floor Number defination
+        /// <summary>
+        /// Current elevator floor
+        /// </summary>
         private int currentFloor;
-        private int defaultFloor = 1;
-        private int mFloor;
 
+        /// <summary>
+        /// Recieved floor index
+        /// </summary>
+        private int recievedFloorIndex;
+
+        /// <summary>
+        /// Recieved data from arduino
+        /// </summary>
         private string recievedData;
-        //private string buttonState;
 
-        private bool goingUp = false;
-        private bool goingDown = false;
+        /// <summary>
+        /// Determines elevator's current state
+        /// </summary>
+        private ElevatorState elevatorState = ElevatorState.Idle;
+
         public Form1()
         {
-            InitializeComponent();
-            //init(); // connect to arduino
-            currentFloor = defaultFloor;
-            label8.Text = currentFloor.ToString();
-            //label4.Text = recievedData;
+            this.InitializeComponent();
+            this.init();
+            this.currentFloor = FirstFloorIndex;
+            this.elevatorState = ElevatorState.Idle;
+            this.label8.Text = currentFloor.ToString();
+            this.label4.Text = recievedData;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// Connects to arduino
+        /// </summary>
         private void init()
         {
             try
@@ -64,244 +86,183 @@ namespace Task2_Elevator
             }
             catch (Exception)
             {
-                MessageBox.Show("Error Connecting Arduino");
+                MessageBox.Show("Error Connecting Arduino!");
+            } 
+        }
+
+        private void myPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.recievedData = myPort.ReadLine();
+        }
+
+        /// <summary>
+        /// Controls elevator's movement logic
+        /// </summary>
+        /// <param name="headFloor"></param>
+        private void MoveElevator(int headFloor)
+        {
+            int floorDiff = Math.Abs(this.currentFloor - headFloor);
+
+            this.DecideElevatorState();
+
+            for (int i=0; i < floorDiff; i++)
+            {
+                if (this.elevatorState == ElevatorState.Up)
+                {
+                    this.currentFloor++;
+                    label8.Text = this.currentFloor.ToString();
+                }
+                else if (this.elevatorState == ElevatorState.Down)
+                {
+                    this.currentFloor--;
+                    label8.Text = this.currentFloor.ToString();
+                }
+
+                this.TimeDelay();
             }
-            
+
+            this.label6.Text = "Stopped";
+            this.ChangeButtonsState();
+        }
+
+        /// <summary>
+        /// Decides current elecator state based on the current floor
+        /// </summary>
+        private void DecideElevatorState()
+        {
+            if (this.currentFloor < headFloor)
+            {
+                this.elevatorState = ElevatorState.Up;
+                this.label6.Text = "Going Up";
+            }
+            else if (this.currentFloor > headFloor)
+            {
+                this.elevatorState = ElevatorState.Down;
+                this.label6.Text = "Going Down";
+            }
+            else
+            {
+                this.elevatorState = ElevatorState.Idle;
+                MessageBox.Show("Destination reached!");
+            }
+        }
+
+        /// <summary>
+        /// Added a specific time delay after finishing an operation
+        /// </summary>
+        public void TimeDelay()
+        {
+            Application.DoEvents();
+
+            if (this.elevatorState == ElevatorState.Up)
+            {
+                label6.Text = "Going Up";
+            }
+            else if (this.elevatorState == ElevatorState.Down)
+            {
+                label6.Text = "Going Down";
+            }
+
+            Thread.Sleep(2000);
+        }
+
+        /// <summary>
+        /// Change buttons state based on the current floor
+        /// </summary>
+        private void ChangeButtonsState()
+        {
+            this.upBtn.Enabled = !(this.currentFloor == 5);
+            this.downBtn.Enabled = !(this.currentFloor == 1);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MoveElevator(1);
-        }
-
-        private void MoveElevator(int headFloor)
-        {
-            //get the floor difference
-            int floorDiff = Math.Abs(currentFloor - headFloor);
-            //decide which direction to go
-            if (currentFloor < headFloor)
-            {
-                goingUp = true;
-                label6.Text = "Going Up";
-            }
-            else if (currentFloor > headFloor)
-            {
-                goingDown = true;
-                label6.Text = "Going Down";
-            }
-            else
-            {
-                MessageBox.Show("Already Here");
-            }
-
-            for (int i=0; i<floorDiff; i++)
-            {
-
-                if (goingUp)
-                {
-                    //increase of decrease floor num
-                    currentFloor++;
-                    //get the current floor
-                    label8.Text = currentFloor.ToString();
-                }
-                else if (goingDown)
-                {
-                    //decrease of decrease floor num
-                    currentFloor--;
-                    //get the current floor
-                    label8.Text = currentFloor.ToString();
-                }
-                //delay 2 sec
-                timeDelay();
-            }
-            goingDown = false;
-            goingUp = false;
-            label6.Text = "Stopped";
-            ButtonControlCheck();
-        }
-
-        public void timeDelay()
-        {
-            Application.DoEvents();
-            if (goingUp)
-            {
-                label6.Text = "Going Up";
-            }
-            else if (goingDown)
-            {
-                label6.Text = "Going Down";
-            }
-            //delay
-            Thread.Sleep(2000);
-        }
-
-        private void ButtonControlCheck()
-        {
-            if (currentFloor == 5)
-            {
-                upBtn.Enabled = false;
-            }
-            else
-            {
-                upBtn.Enabled = true;
-            }
-
-            if (currentFloor == 1)
-            {
-                downBtn.Enabled = false;
-            }
-            else
-            {
-                downBtn.Enabled = true;
-            }
+            this.MoveElevator(FirstFloorIndex);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MoveElevator(2);
+            this.MoveElevator(SecondFloorIndex);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            MoveElevator(3);
+            this.MoveElevator(ThirdFloorIndex);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            MoveElevator(4);
+            this.MoveElevator(ForthFloorIndex);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            MoveElevator(5);
+            this.MoveElevator(FifthFloorIndex);
         }
 
+        /// <summary>
+        /// Up button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button6_Click(object sender, EventArgs e)
         {
-            //up btn
             try
             {
-                //mFloor = int.Parse(textBox1.Text);
-                mFloor = int.Parse(recievedData);
+                this.recievedFloorIndex = int.Parse(this.recievedData);
 
-                if (mFloor <= 5 && mFloor >= 1)
+                if (this.recievedFloorIndex <= 5 && this.recievedFloorIndex >= 1)
                 {
-                    if (currentFloor < mFloor)
+                    if (this.currentFloor > this.recievedFloorIndex)
                     {
-                        CallElevator(mFloor);
+                        this.MessageBox.Show("Can't do this");
+                        return;
                     }
-                    else
-                    {
-                        MessageBox.Show("Can't do this");
-                    }
+
+                    this.MoveElevator(this.recievedFloorIndex);
                 }
                 else
                 {
-                    MessageBox.Show("Please Enter the right number");
+                    MessageBox.Show("Please Enter the right number...");
                 }
             }
             catch (Exception)
             {
-                MessageBox.Show("Please enter the number of your floor");
+                MessageBox.Show("Please enter the number of your floor...");
             }
         }
 
+        /// <summary>
+        /// Down button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button7_Click(object sender, EventArgs e)
         {
-            //down btn
             try
             {
-                //mFloor = int.Parse(textBox1.Text);
-                mFloor = int.Parse(recievedData);
+                this.recievedFloorIndex = int.Parse(this.recievedData);
 
-                if (mFloor <= 5 && mFloor >= 1)
+                if (this.recievedFloorIndex <= 5 && recievedFloorIndex >= 1)
                 {
-                    if(currentFloor > mFloor)
-                    {
-                        CallElevator(mFloor);
-                    }
-                    else
+                    if(this.currentFloor < this.recievedFloorIndex)
                     {
                         MessageBox.Show("Can't do this");
+                        return;
                     }
+
+                    this.MoveElevator(this.recievedFloorIndex);
                 }
                 else
                 {
-                    MessageBox.Show("Please Enter the right number");
+                    MessageBox.Show("Please Enter the right number...");
+                    return;
                 }
             }
             catch (Exception)
             {
-                MessageBox.Show("Please enter the number of your floor");
-            }
-        }
-
-        private void CallElevator(int mFloor)
-        {
-            //get the floor difference
-            int floorDiff = Math.Abs(currentFloor - mFloor);
-            //decide which direction to go
-            if (currentFloor < mFloor)
-            {
-                goingUp = true;
-                label6.Text = "Going Up";
-            }
-            else if (currentFloor > mFloor)
-            {
-                goingDown = true;
-                label6.Text = "Going Down";
-            }
-            else
-            {
-                MessageBox.Show("Already Here");
-            }
-
-            for (int i = 0; i < floorDiff; i++)
-            {
-
-                if (goingUp)
-                {
-                    //increase of decrease floor num
-                    currentFloor++;
-                    //get the current floor
-                    label8.Text = currentFloor.ToString();
-                }
-                else if (goingDown)
-                {
-                    //decrease of decrease floor num
-                    currentFloor--;
-                    //get the current floor
-                    label8.Text = currentFloor.ToString();
-                }
-                //delay 2 sec
-                timeDelay();
-            }
-            goingDown = false;
-            goingUp = false;
-            CheckStatus();
-            ButtonControlCheck();
-            label6.Text = "Stopped";
-        }
-        private void CheckStatus()
-        {
-            //Maintain Up Btn Status
-            if (currentFloor > mFloor)
-            {
-                upBtn.Enabled = false;
-            }
-            else
-            {
-                upBtn.Enabled = true;
-            }
-
-            //Maintain Down Btn Status
-            if (currentFloor < mFloor)
-            {
-                downBtn.Enabled = false;
-            }
-            else
-            {
-                downBtn.Enabled = true;
+                MessageBox.Show("Please enter the number of your floor...");
+                return;
             }
         }
 
@@ -316,17 +277,11 @@ namespace Task2_Elevator
             {
                 myPort.Open();
                 label3.Text = "Status: Connected";
-                //label4.Text = recievedData;
             }
             catch (Exception)
             {
-                MessageBox.Show("Error Connecting Arduino");
+                MessageBox.Show("Error Connecting Arduino!");
             }
-        }
-
-        private void myPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            recievedData = myPort.ReadLine();
         }
 
         private void button7_Click_1(object sender, EventArgs e)
@@ -338,23 +293,8 @@ namespace Task2_Elevator
             }
             catch (Exception)
             {
-                MessageBox.Show("Error Disconnecting Arduino");
+                MessageBox.Show("Error Disconnecting Arduino!");
             }
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
